4539ead2b5933ebf7c5d5784ce58dd2a21e27a0b
ViniciusHP <viniciushp97@gmail.com>
2021-06-14 21:24:21 -0300

    Adicionada a conversão de binário para decimal

--- src/app/conversor/conversor.service.ts
+++ src/app/conversor/conversor.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class ConversorService {
 
   constructor() { }
+
+  converterBinarioParaDecimal(valBinario: string): Promise<string>{
+    return new Promise((resolve, reject) => {
+        const base = 2;
+        let expoente = 0;
+        const indiceFinal = valBinario.length - 1;
+        let soma = 0;
+
+        for(let indice = indiceFinal; indice >= 0; indice--) {
+          const algarismo = Number(valBinario.charAt(indice));
+          soma += Math.pow(base, expoente) * algarismo;
+          expoente++;
+        }
+
+        resolve(soma.toString());
+    });
+  }
 }
