429f075495799d28475effa61614a90603e90358
ViniciusHP <viniciushp97@gmail.com>
2021-06-19 21:36:50 -0300

    Alterado escopo do mapeamentoDecimalParaHexadecimal

--- src/app/conversor/conversor-helper.ts
+++ src/app/conversor/conversor-helper.ts
@@ -9,6 +9,25 @@ const mapeamentoOctalParaBinario: { [key: string]: string } = {
   '7': '111',
 };
 
+const mapeamentoDecimalParaHexadecimal:  { [key: string]: string }  = {
+  '0': '0',
+  '1': '1',
+  '2': '2',
+  '3': '3',
+  '4': '4',
+  '5': '5',
+  '6': '6',
+  '7': '7',
+  '8': '8',
+  '9': '9',
+  '10': 'A',
+  '11': 'B',
+  '12': 'C',
+  '13': 'D',
+  '14': 'E',
+  '15': 'F'
+};
+
 export function inverterOrdemDosNumeros(numero: string): string {
   let numeroInvertido = '';
   for(let index = numero.length - 1; index >= 0; index--){
@@ -18,27 +37,7 @@ export function inverterOrdemDosNumeros(numero: string): string {
 }
 
 export function converterAlgarismoDecimalParaHexadecimal(digitoDecimal: string): string {
-
-  const conjuntoHexadecimal:  { [key: string]: string }  = {
-    '0': '0',
-    '1': '1',
-    '2': '2',
-    '3': '3',
-    '4': '4',
-    '5': '5',
-    '6': '6',
-    '7': '7',
-    '8': '8',
-    '9': '9',
-    '10': 'A',
-    '11': 'B',
-    '12': 'C',
-    '13': 'D',
-    '14': 'E',
-    '15': 'F'
-  }
-
-  let valor = conjuntoHexadecimal[digitoDecimal];
+  let valor = mapeamentoDecimalParaHexadecimal[digitoDecimal];
 
   if(!valor) {
     throw new Error(`O digito '${digitoDecimal}' não é um dígito válido. `);
