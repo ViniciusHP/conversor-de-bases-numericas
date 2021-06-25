ef6524c1c5bbe68ce258c404999596e0903d5f42
ViniciusHP <viniciushp97@gmail.com>
2021-06-24 21:30:49 -0300

    Removida classe que foi substituída pelo uso do ReactiveForms

--- src/app/conversor/tela-conversor/tela-conversor.component.ts
+++ src/app/conversor/tela-conversor/tela-conversor.component.ts
@@ -2,15 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConversorService } from '../conversor.service';
 
-export class DadoDeCoversao {
-  constructor(
-    public baseInicial?: string,
-    public baseFinal?: string,
-    public valorInicial?: string,
-    public valorFinal?: string
-  ) {}
-}
-
 interface FuncaoQueDevolvePromise {
   (valor: string): Promise<string>;
 }
