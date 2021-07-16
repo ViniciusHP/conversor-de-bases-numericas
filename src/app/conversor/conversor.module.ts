8e7f7d6a717842756efb2d818881a520b510a562
ViniciusHP <viniciushp97@gmail.com>
2021-07-15 21:28:07 -0300

    Removido o componente tela-conversor

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -5,22 +5,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
-import {MessagesModule} from 'primeng/messages';
-import {MessageModule} from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
-import { TelaConversorComponent } from './tela-conversor/tela-conversor.component';
 import { ConversorService } from './conversor.service';
 import { FormularioConversorComponent } from './formulario-conversor/formulario-conversor.component';
 import { HistoricoConversorComponent } from './historico-conversor/historico-conversor.component';
 import { CardHistoricoComponent } from './card-historico/card-historico.component';
 
-
 @NgModule({
   declarations: [
-    TelaConversorComponent,
     FormularioConversorComponent,
     HistoricoConversorComponent,
-    CardHistoricoComponent
+    CardHistoricoComponent,
   ],
   imports: [
     CommonModule,
@@ -31,9 +28,9 @@ import { CardHistoricoComponent } from './card-historico/card-historico.componen
     DropdownModule,
     ButtonModule,
     MessagesModule,
-    MessageModule
+    MessageModule,
   ],
-  exports: [TelaConversorComponent],
-  providers: [ConversorService]
+  exports: [FormularioConversorComponent, HistoricoConversorComponent],
+  providers: [ConversorService],
 })
-export class ConversorModule { }
+export class ConversorModule {}
