72f3e68133f44aedfdc74d455e57dc6bffb59f16
ViniciusHP <viniciushp97@gmail.com>
2021-07-19 21:44:44 -0300

    Adicionado header e sidenav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {SidebarModule} from 'primeng/sidebar';
+
 import { FooterComponent } from './footer/footer.component';
-
-
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    FooterComponent
+    FooterComponent,
+    SideBarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    SidebarModule
   ],
   exports: [
-    FooterComponent
+    FooterComponent,
+    SideBarComponent
   ]
 })
 export class CoreModule { }
