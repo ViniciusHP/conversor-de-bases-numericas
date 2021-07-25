cef699508010c92a55a3a91f991015bc819522ce
ViniciusHP <viniciushp97@gmail.com>
2021-07-24 21:52:54 -0300

    Adicionado animação da lista de cards de histórico

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
+import { animate, animateChild, query, sequence, stagger, state, style, transition, trigger } from "@angular/animations";
 
 export const slideDown = trigger('slideDown', [
   state('void', style({ transform: 'translateY(-20px)' , opacity: 0 })),
@@ -18,7 +18,30 @@ export const slideUp = trigger('slideUp', [
 
 export const fade = trigger('fade', [
   state('void', style({ opacity: 0 })),
-  transition(':enter, :leave', [
-    animate(500)
+  transition(':enter', [
+    sequence([
+      animate(500),
+      query('@*', animateChild(), { optional: true })
+    ])
+  ]),
+  transition(':leave', [
+    sequence([
+      query('@*', animateChild(), { optional: true }),
+      animate(500)
+    ])
+  ]),
+])
+
+export const listAnimation = trigger('listAnimation', [
+  transition('* => *', [
+    query(':enter', style({ transform: 'translateY(-20px)', opacity: 0 }), { optional: true }),
+
+    query(':enter', stagger('300ms', [
+      animate('0.7s ease-in-out', style({ transform: 'translateY(0)', opacity: 1 }))
+    ]), { optional: true }),
+
+    query(':leave', stagger('100ms', [
+      animate('0.7s ease-in-out', style({ transform: 'translateX(70px)', opacity: 0 }))
+    ]), { optional: true })
   ])
 ])
