ccfb68b397f67453bce28c28b643c7eecc2b64f2
ViniciusHP <viniciushp97@gmail.com>
2021-07-30 21:29:33 -0300

    Criado shared module e componente desfazer

---
+++ src/app/shared/desfazer/desfazer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desfazer',
+  templateUrl: './desfazer.component.html',
+  styleUrls: ['./desfazer.component.css']
+})
+export class DesfazerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DesfazerComponent } from './desfazer/desfazer.component';
+
+
+
+@NgModule({
+  declarations: [
+    DesfazerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
