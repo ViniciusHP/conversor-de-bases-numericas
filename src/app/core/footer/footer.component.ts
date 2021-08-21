3606d5cd5b39aa3a1f3d8fcc740eada85fc61268
ViniciusHP <viniciushp97@gmail.com>
2021-08-21 17:04:51 -0300

    Comentado código de FooterComponent

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { hoverColor } from 'src/app/animations/animations';
 
+/**
+ * Componente do rodapé da aplicação.
+ */
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
   animations: [hoverColor]
 })
-export class FooterComponent implements OnInit {
-
+export class FooterComponent{
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
